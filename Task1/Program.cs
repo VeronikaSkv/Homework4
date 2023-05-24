254eab18c6a67d710336feaa107934f7cf209534
Veronika <16nika94@mail.ru>
2023-05-24 22:38:55 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите целое число А");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число В");
+int B = Convert.ToInt32(Console.ReadLine());
+int С = A;
+int result = 0;
+for (int i=1; i<B; i++)
+{
+    С = С*A;
+    result = С;
+
+}
+
+Console.WriteLine(result);
