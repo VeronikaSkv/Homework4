15a31b26195df3c6da9bbe75f08251376c75ce6f
Veronika <16nika94@mail.ru>
2023-05-24 22:46:31 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("Введите целое число");
+int[] number = new int [8];
+Random rand = new Random();
+for (int i =0; i<number.Length; i++)
+{
+    number[i] = rand.Next (1, 100);
+ Console.Write(number[i]+",");
+}
